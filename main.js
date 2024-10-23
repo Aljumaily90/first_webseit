3602d242741b2bb0b0036261ced6a8ff65e2523b
Aljumaily90 <a.alju@mail.ch>
2024-10-23 21:45:00 +0200

    finisch01

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     var navbar = $('.navbar');
+    var logo = $('#logo');
     var lastScrollTop = 0;
+    navbar.addClass('transparent'); // Set the navbar to transparent on load
 
     $(window).scroll(function() {
         var currentScroll = $(this).scrollTop();
@@ -15,12 +17,14 @@ $(document).ready(function() {
         
         // Entferne den Hintergrund (transparent) wenn nach unten gescrollt wird
         if (currentScroll > 100) {
-            navbar.removeClass('transparent');
-        } else {
             navbar.addClass('transparent');
+            logo.addClass('large'); // Increase logo size when scrolled
+        } else {
+            navbar.removeClass('transparent');
+            
+            logo.removeClass('large'); // Reset logo size when at top
         }
 
         lastScrollTop = currentScroll;
     });
 });
-
